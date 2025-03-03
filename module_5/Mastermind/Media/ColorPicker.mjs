71b805ba13e5dd22d72bb1b2f6eb47d9e4eefe7e
theaaeh30 <tahansen@uia.no>
2025-03-03 08:57:00 +0100

    Får se nærmere på dette

--- module_5/Mastermind/Media/ColorPicker.mjs
+++ module_5/Mastermind/Media/ColorPicker.mjs
@@ -11,8 +11,6 @@ export class TColorPicker extends libSprite.TSpriteDraggable {
   #spcvs;
   #spi;
   #color;
-  #snapPos;
-  #hasMoved;
   constructor(spcvs, spriteInfo, color, index){
     super(spcvs, spriteInfo,Positions[color]);
     this.index = index;
@@ -20,8 +18,6 @@ export class TColorPicker extends libSprite.TSpriteDraggable {
     this.#spcvs = spcvs;
     this.#spi = spriteInfo;
     this.#color = color;
-    this.#snapPos = null;
-    this.#hasMoved = false;
   }
 
   onCanDrop(){
@@ -37,34 +33,8 @@ export class TColorPicker extends libSprite.TSpriteDraggable {
     )
   }
 
-  onDrop(aDropPosition){
+  onDrop(){
     GameProps.colorPickers.push(this.clone());
-    const index = GameProps.snapTo.positions.indexOf(aDropPosition);
-    const removedItems = GameProps.snapTo.positions.splice(index, 1);
-    this.#snapPos = removedItems[0];
-    this.#hasMoved = true;
   }
 
-  onMouseDown(){
-    super.onMouseDown();
-    //Få denne knappen til å være i det øverste laget
-    const index = GameProps.colorPickers.indexOf(this);
-    GameProps.colorPickers.splice(index, 1);
-    GameProps.colorPickers.push(this);
-    if(this.#snapPos !== null){
-      console.log("Pushing snapPos", this.#snapPos);
-      GameProps.snapTo.positions.push(this.#snapPos);
-      this.#snapPos = null;
-    }
-  }
-
-  onCancelDrop(){
-    //Fjern denne knappen fra listen over knapper.
-    //Først finn indeksen til denne knappen
-    //Deretter fjern knappen fra listen
-    if(this.#hasMoved){
-      const index = GameProps.colorPickers.indexOf(this);
-      GameProps.colorPickers.splice(index, 1);
-      this.spcvs.removeSpriteButton(this); 
-    }
-  }
+} 
